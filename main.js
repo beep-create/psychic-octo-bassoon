bd171c708f1f8eb81077c88b34205be8486773d7
beep-create <77546458+beep-create@users.noreply.github.com>
2021-12-03 17:39:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 video="";
 status="";
 objects=[];
-
+object_to_detect="";
+synth = window.speechSynthesis;
 
 function change_background() {
     document.body.style.backgroundImage = "url('camera.jpg')";
@@ -11,7 +12,7 @@ function change_background() {
 }
 
 function preload() {
-    video=createVideo('video.mp4');
+    video=createVideo('the_video.mp4');
     video.hide();
 
 }
@@ -27,22 +28,28 @@ function draw() {
    if(status != "") {
         objectDetector.detect(video, gotResults);
         for (i = 0; i < objects.length; i++) {
-            document.getElementById("status").innerHTML = "Status: Objects detected!";
-            document.getElementById("number_of_objects").innerHTML = "Number of objects: " + objects.length;
             fill("#FF5454");
             percent=floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%" , objects[i].x+15, objects[i].y+15);
             noFill();
             stroke("#FF5454");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-
+              
 
         }
-
    }
 
+   if(object_to_detect == document.getElementById("input").innerHTML) {
+    video.stop();
+    objectDetector.detect(gotResults);
+    document.getElementById("status").innerHTML = "Status: Objects mentioned found!";
+    SpeechSynthesisUtterance("object mentioned found");
+    synth.speak(utterThis);
 
-
+   }
+  else {
+    document.getElementById("status").innerHTML = "Status: Object(s) mentioned not found..";
+  }
 
 }
 
